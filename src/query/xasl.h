a4fa809fd74766fa3db5392d7f166ba7c2ef51f7
정소희 <94791489+sohee-dgist@users.noreply.github.com>
2025-03-13 12:33:11 +0900

    Update src/query/xasl.h
    Co-authored-by: SE park <shpark@cubrid.com>

--- src/query/xasl.h
+++ src/query/xasl.h
@@ -55,7 +55,7 @@
 struct binary_heap;
 #endif // SERVER_MODE || SA_MODE
 
-#define UNPACK_SCALE 3		// change stx_init_xasl_unpack_info too
+#define UNPACK_SCALE 3		// Assumed memory ratio when unpacking stream to XASL
 
 struct xasl_node;
 typedef struct xasl_node XASL_NODE;
