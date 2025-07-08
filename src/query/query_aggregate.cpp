e652e05b560185523f078a117aec9e857420ad77
SOHEE_JUNG <soheejung@cubrid.com>
2025-07-08 17:36:47 +0900

    (bugfix) min/max 최적화 continue 위치 잘못된 부분 수정

--- src/query/query_aggregate.cpp
+++ src/query/query_aggregate.cpp
@@ -692,6 +692,7 @@ qdata_evaluate_aggregate_list (cubthread::entry *thread_p, cubxasl::aggregate_li
 		      pr_clone_value (&db_values[0], accumulator->value);
 		    }
 		  agg_p->is_ended = true;
+		  continue;
 		}
 
 	      break;
@@ -716,6 +717,7 @@ qdata_evaluate_aggregate_list (cubthread::entry *thread_p, cubxasl::aggregate_li
 		      pr_clone_value (&db_values[0], accumulator->value);
 		    }
 		  agg_p->is_ended = true;
+		  continue;
 		}
 	      break;
 
@@ -723,7 +725,6 @@ qdata_evaluate_aggregate_list (cubthread::entry *thread_p, cubxasl::aggregate_li
 	      assert (false);
 	      break;
 	    }
-	  continue;
 	}
 
       /*
