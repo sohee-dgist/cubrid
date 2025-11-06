bab60daf63666affe5a167c0fb33365a33ef5d5b
SOHEE_JUNG <soheejung@cubrid.com>
2025-11-06 21:00:32 +0900

    (fix) CBRD-26217: histogram class에 mcv 추가

---
+++ src/histogram/histogram_cl.cpp
@@ -0,0 +1,235 @@
+#include "dbtype_def.h"
+#include "histogram_cl.hpp"
+#include "db.h"
+#include "histogram_builder.hpp"
+#include "thread_compat.hpp"
+#include "db_query.h"
+#include "locator_cl.h"
+#include "schema_manager.h"
+#include "schema_system_catalog_constants.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+ * analyze_all_classes
+ *
+ * return:
+ *   with_fullscan(in): true iff WITH FULLSCAN
+ *
+ * NOTE:
+ */
+int
+analyze_classes (THREAD_ENTRY *thread_p, const char *tbl_name, const char *attr_name, int max_number_of_buckets,
+		 int with_fullscan, MOP classop)
+{
+  int error = NO_ERROR;
+  char *histogram_blob = NULL;
+  int histogram_total_length = 0;
+  error =
+	  get_histogram (thread_p, tbl_name, attr_name, max_number_of_buckets, with_fullscan, &histogram_blob,
+			 &histogram_total_length);
+  if (error != NO_ERROR)
+    {
+      return error;
+    }
+  error = set_histogram (thread_p, tbl_name, attr_name, histogram_blob, histogram_total_length, classop);
+  if (error != NO_ERROR)
+    {
+      return error;
+    }
+  db_private_free (thread_p, histogram_blob);
+
+  return NO_ERROR;
+}
+
+int
+get_histogram (THREAD_ENTRY *thread_p, const char *tbl_name, const char *attr_name, int max_number_of_buckets,
+	       int with_fullscan, char **histogram_blob, int *histogram_total_length)
+{
+  int error = NO_ERROR;
+  DB_QUERY_RESULT *query_result;
+  DB_QUERY_ERROR query_error;
+  hist::HistogramBuilder histogram_builder;
+  DB_TYPE type = DB_TYPE_UNKNOWN;
+  bool sampling_scan = false;
+  int number_of_mcv = 3;	// TODO
+
+  char query_buf[1024];
+  if (sampling_scan)
+    {
+      snprintf (query_buf, sizeof (query_buf), HISTOGRAM_WITH_SAMPLING_SCAN_QUERY_TEMPLATE, attr_name, tbl_name,
+		attr_name, number_of_mcv, max_number_of_buckets, max_number_of_buckets);
+    }
+  else
+    {
+      snprintf (query_buf, sizeof (query_buf), HISTOGRAM_QUERY_TEMPLATE, attr_name, tbl_name, attr_name,
+		number_of_mcv, max_number_of_buckets, max_number_of_buckets);
+    }
+
+  error = db_compile_and_execute_local (query_buf, &query_result, &query_error);
+
+  if (error < 0)
+    {
+      return error;
+    }
+
+  error = db_query_first_tuple (query_result);
+  if (error != DB_CURSOR_SUCCESS)
+    {
+      if (error == DB_CURSOR_END)
+	{
+	  error = NO_ERROR;
+	}
+      else
+	{
+	  ASSERT_ERROR ();
+	}
+      return error;
+    }
+
+
+  do
+    {
+      DB_VALUE value[5];
+      error = db_query_get_tuple_value_by_name (query_result, const_cast < char *> ("bid"), &value[0]);
+      error = db_query_get_tuple_value_by_name (query_result, const_cast < char *> ("endpoint"), &value[1]);
+      error = db_query_get_tuple_value_by_name (query_result, const_cast < char *> ("rows_in_bucket"), &value[2]);
+      error = db_query_get_tuple_value_by_name (query_result, const_cast < char *> ("cumulative"), &value[3]);
+      error = db_query_get_tuple_value_by_name (query_result, const_cast < char *> ("approx_ndv"), &value[4]);
+
+      if (error != NO_ERROR)
+	{
+	  return error;
+	}
+
+      switch (value[1].domain.general_info.type)
+	{
+	case DB_TYPE_INTEGER:
+	{
+	  // int를 std::int64_t로 변환하여 variant 생성
+	  hist::HistogramTypes hi = static_cast < std::int64_t > (db_get_int (&value[1]));
+	  histogram_builder.add (hi, db_get_bigint (&value[3]), db_get_bigint (&value[4]));
+	}
+	type = DB_TYPE_INTEGER;
+	break;
+	case DB_TYPE_BIGINT:
+	{
+	  // int64_t를 variant로 생성
+	  std::int64_t val = db_get_bigint (&value[1]);
+	  hist::HistogramTypes hi {val};
+	  histogram_builder.add (hi, db_get_bigint (&value[3]), db_get_bigint (&value[4]));
+	}
+	type = DB_TYPE_BIGINT;
+	break;
+	case DB_TYPE_DOUBLE:
+	{
+	  // double을 variant로 생성
+	  double val = db_get_double (&value[1]);
+	  hist::HistogramTypes hi {val};
+	  histogram_builder.add (hi, db_get_bigint (&value[3]), db_get_bigint (&value[4]));
+	}
+	type = DB_TYPE_DOUBLE;
+	break;
+	case DB_TYPE_STRING:
+	{
+	  // string을 variant로 생성 (복사 생성으로 안전하게)
+	  const char *str = db_get_string (&value[1]);
+	  if (str == NULL)
+	    {
+	      return ER_FAILED;
+	    }
+	  std::string str_val (str);	// 복사 생성 - 안전
+	  hist::HistogramTypes hi {str_val};
+	  histogram_builder.add (hi, db_get_bigint (&value[3]), db_get_bigint (&value[4]));
+	}
+	type = DB_TYPE_STRING;
+	break;
+	default:
+	  assert (false);
+	  break;
+	}
+    }
+  while (db_query_next_tuple (query_result) == DB_CURSOR_SUCCESS);
+
+  *histogram_blob = histogram_builder.build (thread_p, type, histogram_total_length);
+  if (*histogram_blob == NULL)
+    {
+      return ER_FAILED;
+    }
+
+  return NO_ERROR;
+}
+
+int
+set_histogram (THREAD_ENTRY *thread_p, const char *tbl_name, const char *attr_name, char *histogram_blob,
+	       int histogram_total_length, MOP classop)
+{
+  int error = NO_ERROR;
+  DB_OBJECT *histogram_class, *histogram_obj, *edit_histogram_object = NULL;
+  DB_OTMPL *obj_tmpl = NULL;
+  DB_VALUE value[2];
+  DB_VALUE *value_ptrs[2] = { &value[0], &value[1] };
+  DB_VALUE histogram_value;
+  const char *search_attrs[2] = { "class_of", "key_attr" };
+
+  histogram_class = sm_find_class (CT_DB_HISTOGRAM_NAME);
+  if (histogram_class == NULL)
+    {
+      error = ER_BO_MISSING_OR_INVALID_CATALOG;
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, error, 0);
+      goto end;
+    }
+
+  /* class_of, key_attr */
+  db_make_object (&value[0], classop);
+  db_make_string (&value[1], attr_name);
+
+  histogram_obj = db_find_multi_unique (histogram_class, 2, (char **) search_attrs, value_ptrs, DB_FETCH_READ);
+  if (histogram_obj == NULL)
+    {
+      error = ER_LC_CLASSNAME_EXIST;
+      char error_histogram[256];
+      sprintf (error_histogram, "histogram of %s(%s)", sm_get_ch_name (classop), attr_name);
+      er_set (ER_ERROR_SEVERITY, ARG_FILE_LINE, error, 1, error_histogram);
+      goto end;
+    }
+
+  obj_tmpl = dbt_edit_object (histogram_obj);
+  if (obj_tmpl == NULL)
+    {
+      assert (er_errid () != NO_ERROR);
+      error = er_errid ();
+      goto end;
+    }
+
+  db_make_varbit (&histogram_value, 1073741823, histogram_blob, histogram_total_length);
+  error = dbt_put (obj_tmpl, "histogram_values", &histogram_value);
+  if (error != NO_ERROR)
+    {
+      goto end;
+    }
+
+  edit_histogram_object = dbt_finish_object (obj_tmpl);
+  if (edit_histogram_object == NULL)
+    {
+      assert (er_errid () != NO_ERROR);
+      error = er_errid ();
+      goto end;
+    }
+
+  assert (edit_histogram_object == histogram_obj);
+  obj_tmpl = NULL;
+
+  error = locator_flush_instance (edit_histogram_object);
+  if (error != NO_ERROR)
+    {
+      goto end;
+    }
+
+end:
+  db_value_clear (value_ptrs[0]);
+  db_value_clear (value_ptrs[1]);
+  db_value_clear (&histogram_value);
+  assert (error == NO_ERROR);	// for debug
+  return error;
+}
---
+++ src/histogram/histogram_cl.hpp
@@ -0,0 +1,32 @@
+#include "thread_compat.hpp"
+static const char *HISTOGRAM_QUERY_TEMPLATE =
+	"WITH src AS (SELECT %s AS val FROM %s WHERE %s IS NOT NULL), "
+	"cnt AS (SELECT val, COUNT(*) AS c FROM src GROUP BY val), "
+	"mcv_ranked AS (SELECT val, c, ROW_NUMBER() OVER (ORDER BY c DESC, val) AS rn FROM cnt ORDER BY c DESC LIMIT %d), "
+	"non_mcv_flagged AS (SELECT val, c FROM cnt WHERE val NOT IN (SELECT val FROM mcv_ranked)), "
+	"hist_acc AS (SELECT val, c, SUM(c) OVER (ORDER BY val) AS cum, SUM(c) OVER () AS n FROM non_mcv_flagged), "
+	"param AS (SELECT CASE WHEN n > 0 THEN CEIL(n * 1.0 / %d) ELSE 1 END AS cap, n FROM hist_acc LIMIT 1), "
+	"hist_buckets AS (SELECT LEAST(FLOOR((cum - 1) / param.cap), %d - 1) AS bid, val, c, FALSE AS is_mcv FROM hist_acc, param), "
+	"mcv_buckets AS (SELECT -rn AS bid, val, c, TRUE AS is_mcv FROM mcv_ranked), "
+	"all_buckets AS (SELECT * FROM hist_buckets UNION ALL SELECT * FROM mcv_buckets) "
+	"SELECT bid, MAX(val) AS endpoint, SUM(c) AS rows_in_bucket, SUM(SUM(c)) OVER (ORDER BY MAX(val)) AS cumulative, "
+	"COUNT(*) AS approx_ndv, MAX(is_mcv) AS is_mcv FROM all_buckets GROUP BY bid ORDER BY MAX(val);";
+static const char *HISTOGRAM_WITH_SAMPLING_SCAN_QUERY_TEMPLATE =
+	"WITH src AS (SELECT /*+ SAMPLING_SCAN */ %s AS val FROM %s WHERE %s IS NOT NULL), "
+	"cnt AS (SELECT val, COUNT(*) AS c FROM src GROUP BY val), "
+	"mcv_ranked AS (SELECT val, c, ROW_NUMBER() OVER (ORDER BY c DESC, val) AS rn FROM cnt ORDER BY c DESC LIMIT %d), "
+	"non_mcv_flagged AS (SELECT val, c FROM cnt WHERE val NOT IN (SELECT val FROM mcv_ranked)), "
+	"hist_acc AS (SELECT val, c, SUM(c) OVER (ORDER BY val) AS cum, SUM(c) OVER () AS n FROM non_mcv_flagged), "
+	"param AS (SELECT CASE WHEN n > 0 THEN CEIL(n * 1.0 / %d) ELSE 1 END AS cap, n FROM hist_acc LIMIT 1), "
+	"hist_buckets AS (SELECT LEAST(FLOOR((cum - 1) / param.cap), %d - 1) AS bid, val, c, FALSE AS is_mcv FROM hist_acc, param), "
+	"mcv_buckets AS (SELECT -rn AS bid, val, c, TRUE AS is_mcv FROM mcv_ranked), "
+	"all_buckets AS (SELECT * FROM hist_buckets UNION ALL SELECT * FROM mcv_buckets) "
+	"SELECT bid, MAX(val) AS endpoint, SUM(c) AS rows_in_bucket, SUM(SUM(c)) OVER (ORDER BY MAX(val)) AS cumulative, "
+	"COUNT(*) AS approx_ndv, MAX(is_mcv) AS is_mcv FROM all_buckets GROUP BY bid ORDER BY MAX(val);";
+
+int analyze_classes (THREAD_ENTRY *thread_p, const char *tbl_name, const char *attr_name, int max_number_of_buckets,
+		     int with_fullscan, MOP classop);
+int get_histogram (THREAD_ENTRY *thread_p, const char *tbl_name, const char *attr_name, int max_number_of_buckets,
+		   int with_fullscan, char **histogram_blob, int *histogram_total_length);
+int set_histogram (THREAD_ENTRY *thread_p, const char *tbl_name, const char *attr_name, char *histogram_blob,
+		   int histogram_total_length, MOP classop);
