4db5f261405b68cffd35f4beb67d8f4fe2ac5e23
정소희 <94791489+sohee-dgist@users.noreply.github.com>
2026-03-24 15:15:51 +0900

    Update src/query/execute_schema.c
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- src/query/execute_schema.c
+++ src/query/execute_schema.c
@@ -4405,8 +4405,9 @@ do_drop_histogram (PARSER_CONTEXT * parser, PT_NODE * statement)
   PT_NODE *cls;
   DB_OBJECT *obj;
   int error = NO_ERROR, save;
-  AU_DISABLE (save);
+  int error = NO_ERROR, save;
   CHECK_MODIFICATION_ERROR ();
+  AU_DISABLE (save);
 
   /* class should be already available */
   assert (statement->info.histogram.target_table_spec);
